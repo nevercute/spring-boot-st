e33a69d10ebcc410edd9f96db8e1d3928122f30e
Лисичкин Александр <lisichkinas@m2.ru>
2024-12-29 11:43:33 +0300

    init

---
+++ spring-boot-st/src/test/java/pro/nc/springbootst/SpringBootStApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.nc.springbootst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
